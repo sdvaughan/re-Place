f98bbbaeb2550036ba6f48dffd6ca5f0aa3137ae
svaughan <svaughan@purdue.edu>
2019-04-22 22:58:28 -0400

    Began index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<head>
+<style>
+	p.sansserif {
+		font-family: "Agency FB", sans-serif;
+		font-weight: bold;
+	}
+	label {
+		display: inline-block;
+		width: 50px;
+		margin-right: 30px;
+		text-align: right;
+	}
+</style>
+	<h1 style="color: #800000; font-family: Arial;">re/Place</h1>
+</head>
+</html>
